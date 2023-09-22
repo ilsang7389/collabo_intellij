b32b023e7b0f65f415775625924742e099d08aec
Yoon Ilsang <ilsang7389@naver.com>
2023-09-22 10:37:32 +0900

    신규파일추가요

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -1,9 +1,9 @@
 package ch01;
 
 public class Hello {
-    public  static  void Main(String[] args){
+    public static void main(String[] args) {
         System.out.println("hello01");
         System.out.println("hello02");
-        System.out.println("hello02");
+        System.out.println("hello02gg");
     }
 }
